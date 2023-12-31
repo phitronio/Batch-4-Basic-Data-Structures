0166d96f70701a9c962625f4fcf942d379b76bde
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2023-12-31 19:44:56 +0600

    module 18 upload

---
+++ Module 18/count_number_of_nodes.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ano
+        Node *p = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj ache
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        // 3. children gulo ke push koro
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+int count(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    int l = count(root->left);
+    int r = count(root->right);
+    return l + r + 1;
+}
+int main()
+{
+    Node *root = input_tree();
+    cout << count(root) << endl;
+    return 0;
+}
---
+++ Module 18/level_order_print.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "Tree nai" << endl;
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ana
+        Node *f = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj ache
+        cout << f->val << " ";
+
+        // 3. tar children gulo ke rakha
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+}
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+    // connection
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = h;
+    c->right = e;
+    b->right = d;
+    d->left = f;
+    d->right = g;
+    h->right = i;
+
+    level_order(root);
+    return 0;
+}
